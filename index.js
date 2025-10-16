ad0ecdee94ad572478f9bfb7e4a800a3d4f8c3d2
Nicholas Cruz <ncruz@ntracts.com>
2025-10-16 16:01:18 -0400

    Establish app insights setup in app code and track event to see flow

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const appinsights = require("applicationinsights");
+
+appinsights.setup().start();
+const appInsightsClient = appinsights.defaultClient;
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -8,6 +12,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 
 app.get("/", (req, res) => {
+  appInsightsClient.trackEvent({ name: "RootEndpointHit", properties:{message: "Root endpoint was hit"} });
   res.send("Hello, world");
 });
 
